14b77b349d2787832f0bdc7fcfbab9b056d27ee2
Dacrybabysuck <dacrybabysuck@gmail.com>
2021-01-08 11:42:24 -0500

    feat(dbupdate): will now restore table data

--- build.sh
+++ build.sh
@@ -6,11 +6,17 @@ timestamp=$(date +%s%N)
 backupFolder=sql_backup/${timestamp}
 mkdir -p "${backupFolder}"
 
-declare -a BackupTables=("auction_house" "chars" "accounts" "accounts_banned" "char_effects" "char_equip" "char_exp" "char_inventory" "char_jobs" "char_look" "char_merit" "char_pet" "char_points" "char_profile" "char_recast" "char_skills" "char_spells" "char_stats" "char_storage" "char_style" "char_unlocks" "char_vars" "conquest_system" "server_variables" "delivery_box" "linkshells")
-
+declare -a BackupTables=("auction_house" "accounts" "chars" "accounts_banned" "char_effects" "char_equip" "char_exp" "char_inventory" "char_jobs" "char_look" "char_merit" "char_pet" "char_points" "char_profile" "char_recast" "char_skills" "char_spells" "char_stats" "char_storage" "char_style" "char_unlocks" "char_vars" "conquest_system" "server_variables" "delivery_box" "linkshells")
 
 function backupTable {
-     mysqldump -e --hex-blob -hdb --no-create-info -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" "${1}" > "${backupFolder}"/"${1}".sql
+     echo -n "Backing up table ${1}..."
+     mysqldump -e --hex-blob -hdb --skip-triggers --no-create-info -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" "${1}" > "${backupFolder}"/"${1}".sql && echo "Success"
+}
+
+function restoreTable {
+     echo -n "Restoring table ${1}..."
+     mysql -hdb -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" -e "truncate table ${1}"
+     mysql -hdb -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < "${backupFolder}"/"${1}".sql && echo "Success"
 }
 
 for val in ${BackupTables[@]}; do
@@ -18,9 +24,17 @@ for val in ${BackupTables[@]}; do
 done
 
 
-if [[ -e sql/char_merits.sql ]]; then
-  rm sql/char_merits.sql
-fi
+pushd /topaz/sql
+for f in *.sql
+  do
+     echo -n "Importing $f into the database..."
+     mysql -hdb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < "$f" && echo "Success"
+  done
+popd
+
+for val in ${BackupTables[@]}; do
+   restoreTable $val
+done
 
 tar -jcvf "${backupFolder}".tar.bz2 "${backupFolder}"
 
@@ -28,16 +42,11 @@ if [ -f "${backupFolder}".tar.bz2 ]; then
      rm -rf "${backupFolder}"
 fi
 
-cd /topaz/sql
-for f in *.sql
-  do
-     echo -n "Importing $f into the database..."
-     mysql -hdb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < "$f" && echo "Success"
-  done
-
+pushd /topaz/sql
 mysql -hdb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < accounts_sessions.sql && echo "Success"
 mysql -hdb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < accounts_parties.sql && echo "Success"
 mysql -hdb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < triggers.sql && echo "Success"
+popd
 
 if [[ -d /topaz/sql/custom && -f /topaz/scripts/globals/settings.lua ]]; then
   cd /topaz/sql/custom
@@ -48,3 +57,4 @@ if [[ -d /topaz/sql/custom && -f /topaz/scripts/globals/settings.lua ]]; then
      fi
     done
 fi
+
